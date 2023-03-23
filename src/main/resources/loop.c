4c5bb48926c162051b3a55a68fe595551e631f53
ponyma <ponyma@kuainiugroup.com>
2023-03-23 14:03:38 +0800

    添加圆周率，质数 两个C程序样例

--- src/main/resources/loop.c
+++ src/main/resources/loop.c
@@ -1,47 +1,11 @@
-//int main(void)
-//{
-//    unsigned int *tty = (unsigned int *)0x000000FF;
-//    int i = 5,j,flag;
-//    *tty  = i;
-//    unsigned long counter = 0;
-//        for(j=2; j<i; j++)
-//            {
-//                *tty  = j;
-//                *tty  = i%j;
-//                if(i%j == 0){
-//                    flag = 1;
-//                    break;
-//                }
-//            }
-//            *tty = flag;
-//        if(flag == 0) // 判断i是否为质数
-//            {
-//                 *tty = i; // 输出i，表示它是一个质数
-//            }
-//    return 0;
-//}
-
-int main()
+void main(void)
 {
-unsigned int *tty = (unsigned int *)0x000000FF;
-int i, j, flag;
-// 从2枚举到1000，判断每个数是否为质数
-for(i=2; i<=1000; i++)
-{
-    flag = 0; // 标记是否为质数
-    // 在2到i-1之间找一个能整除i的数，如果找到了，就说明i不是质数
-    for(j=2; j<i; j++)
-    {
-        if(i % j == 0)
-        {
-            flag = 1; // i不是质数
-            break;
-        }
-    }
-    if(flag == 0) // 判断i是否为质数
-    {
-         *tty = i; // 输出i，表示它是一个质数
-    }
-}
-return 0;
+	int len = 100;
+	int _sum = 0;
+	for(int i = 0; i <= len; i++){
+	    _sum += i;
+	}
+	//打印地址
+	unsigned int *tty = (unsigned int *)0x000000FF;
+	*tty = _sum;
 }
